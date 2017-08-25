9ed9a53016a93c280aedf32da39c50e575f6dba3
Alexandre Díaz <dev@redneboa.es>
2017-08-25 18:24:38 +0200

    [FIX] leaks on extract process

--- tools/console_packer.cpp
+++ tools/console_packer.cpp
@@ -94,6 +94,7 @@ int main(int argc, char *argv[])
 						std::cout << "Can't extract '" << aExtractContentPath << "'" << std::endl;
 					}
         		}
+                delete [] pData;
         	}
 
         	if (listMode)
