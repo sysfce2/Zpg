324d02ff72d325646679e10c361399ee33bad8bd
Tardo <dev@redneboa.es>
2017-09-03 02:53:11 +0200

    [ADD] zlib

--- cmake/FindZOPFLI.cmake
+++ cmake/FindZOPFLI.cmake
@@ -26,6 +26,4 @@ endif()
 # handle success
 if(ZOPFLI_FOUND)
     message(STATUS "Found ZOPFLI in ${ZOPFLI_INCLUDE_DIR}")
-else()
-    message(STATUS "ZOPFLI Not Found... using 'external' location...")
 endif()
